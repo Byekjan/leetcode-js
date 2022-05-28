7507f2db2d063450dd8095db41fc439ce139f761
byekjan <janarbyek_byekjan@unimedia.co.jp>
2022-05-29 00:49:44 +0800

    two sum

--- twoSum.js
+++ twoSum.js
@@ -8,7 +8,15 @@ var array = [2,7,11,15];
 var target = 9;
 
 var twoSum = function(nums, target) {
-    console.log("ddds");
+    const len = nums.length;
+    const seen = new Map([[nums[0], 0]]);
+    for (let i = 1; i < len; i++) {
+        if (seen.has(target - nums[i])) {
+            return [seen.get(target - nums[i]), i];
+        } else {
+            seen.set(nums[i], i);
+        }
+    }
 };
 
 console.log(twoSum(array, target));
