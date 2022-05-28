71b581794582cdb891286f6cc973b8bff40b0360
byekjan <janarbyek_byekjan@unimedia.co.jp>
2022-05-29 00:26:42 +0800

    solution1

---
+++ twoSum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var array = [2,7,11,15];
+var target = 9;
+
+var twoSum = function(nums, target) {
+    console.log("ds");
+};
+
+console.log(twoSum(array, target));
