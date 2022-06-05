03c0b5151997b5543fd41c8c81c8296116a7d609
byekjan <janarbek.bekjan@gmail.com>
2022-06-05 19:14:12 +0800

    problem 118, 199

--- 108.arrayToBinary.js
+++ 108.arrayToBinary.js
@@ -1,22 +1,29 @@
 nums = [-10,-3,0,5,9];
 
 function TreeNode(val) {
-    console.log(val);
     this.val = val;
     this.left = this.right = null;
 }
 
 var sortedArrayToBST = function(nums, start = 0, end = nums.length-1) {
+    // console.log(start);
+    // console.log(end);
     if(start>end){                        // if start>end means left tree or right subtree is not possible so return null
         return null;
     }
     let mid = Math.floor((start+end)/2);       // get the mid index
-    let root = new TreeNode(nums[mid]);        // make a new node
+    let root = new TreeNode(nums[mid]);        // make a new node   2 
+    // console.log(root);
     root.left = sortedArrayToBST(nums,start,mid-1);    // now recursively generate left subtree
+    // console.log(root.left);
     root.right = sortedArrayToBST(nums,mid+1,end);     // similarly generate right subtree
+    // console.log(root.right);
     return root;    // recursively parse through array
 };
 
 
-console.log(sortedArrayToBST(nums));
+let  arr = sortedArrayToBST(nums);
 
+console.log(arr);
+var my_obj_str = JSON.stringify(arr);
+console.log(my_obj_str);
---
+++ 119.pascal'sTraingle2.js
@@ -0,0 +1,18 @@
+var numRows = 3;
+
+var generate = function(numRows) {
+    const table = [];
+    for (let i = 0; i <= numRows; i++) {
+        table[i] = [];
+        table[i][0] = 1;
+        for (let j = 1; j < i; j++) {
+            table[i][j] = table[i-1][j-1] + table[i-1][j]
+        }
+        table[i][i] = 1;
+    }
+    return table[numRows];
+}
+
+let  arr = generate(numRows);
+
+console.log(arr);
